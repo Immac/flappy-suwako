e97658df339267629ff4ba00754c1da1cd733932
Miguel Chicas <immac.gm@gmail.com>
2025-01-04 21:46:56 -0600

    Update viewport height and add speed variable

--- scenes/onbashira.gd
+++ scenes/onbashira.gd
@@ -1,6 +1,6 @@
 extends Node2D
 @onready var animation_tree: AnimationTree = $Node/AnimationTree
-
+@export var speed := -100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +9,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _physics_process(delta: float) -> void:
+	position.x += speed * delta
